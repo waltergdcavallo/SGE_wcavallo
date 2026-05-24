39ba7964f8e860d029279b3b4e2c148d493d0a7a
Walter <wosinalde@gmail.com>
2026-05-24 12:06:46 -0300

    quedaron dudas con el entityManager

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,9 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;                                         //NUEVA RUTA
+
 
 class DefaultController extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager                            //CONSTRUCTOR NUEVO
+    ){}
+
     #[Route(
         '/sitio/{pagina}',
          name: 'app_estatica',
@@ -20,5 +26,22 @@ class DefaultController extends AbstractController
     public function estatica(string $pagina): Response
     {
         return $this->render('estatica/'.$pagina.'.html.twig');
+    }
+
+    #[Route('/', name: 'portada')]
+    public function portada(): Response
+    {
+        $em = $this->entityManager;
+
+        $eventos = $em->getRepository(Evento::class)->findAll();
+
+        shuffle($eventos);
+
+        $eventos = array_slice($eventos, 0, 8);
+
+        return $this->render('default/portada.html.twig', [
+            'eventosCol1' => array_slice($eventos, 0, 4),
+            'eventosCol2' => array_slice($eventos, 4, 4),
+        ]);
     }
 }
